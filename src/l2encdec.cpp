bc90de79e4195f60dbaf8042b010f40cc47cc9f8
ritsu <discohijackat125ruemontmarte@proton.me>
2025-11-22 20:17:46 +0000

    Fix order

--- src/l2encdec.cpp
+++ src/l2encdec.cpp
@@ -24,8 +24,8 @@ const std::unordered_map<int, l2encdec::Params> PROTOCOL_CONFIGS = {
 const l2encdec::Params MODERN_RSA_PARAMS = {
     .type = l2encdec::Type::RSA,
     .rsa_modulus = "75b4d6de5c016544068a1acf125869f43d2e09fc55b8b1e289556daf9b8757635593446288b3653da1ce91c87bb1a5c18f16323495c55d7d72c0890a83f69bfd1fd9434eb1c02f3e4679edfa43309319070129c267c85604d87bb65bae205de3707af1d2108881abb567c3b3d069ae67c3a4c6a3aa93d26413d4c66094ae2039",
-    .rsa_private_exponent = "1d",
     .rsa_public_exponent = "30b4c2d798d47086145c75063c8e841e719776e400291d7838d3e6c4405b504c6a07f8fca27f32b86643d2649d1d5f124cdd0bf272f0909dd7352fe10a77b34d831043d9ae541f8263c6fe3d1c14c2f04e43a7253a6dda9a8c1562cbd493c1b631a1957618ad5dfe5ca28553f746e2fc6f2db816c7db223ec91e955081c1de65",
+    .rsa_private_exponent = "1d",
 };
 
 inline void insert_header(std::vector<unsigned char> &data, std::string header)
