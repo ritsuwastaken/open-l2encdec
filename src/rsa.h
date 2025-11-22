26ffbf4249d62ee613a90c6ed1fe8c6dd16d1e3a
ritsu <discohijackat125ruemontmarte@proton.me>
2025-11-22 20:35:26 +0000

    Fix `RSA` header

--- src/rsa.h
+++ src/rsa.h
@@ -1,4 +1,6 @@
 #ifndef RSA_H
+#define RSA_H
+
 #include <string>
 #include <vector>
 
@@ -7,6 +9,5 @@ namespace RSA
 void encrypt(const std::vector<unsigned char> &input_data, std::vector<unsigned char> &output_data, const std::string &modulus_hex, const std::string &public_exp_hex);
 int decrypt(const std::vector<unsigned char> &input_data, std::vector<unsigned char> &output_data, const std::string &modulus_hex, const std::string &private_exp_hex);
 } // namespace RSA
-#define RSA_H
 
 #endif // RSA_H
