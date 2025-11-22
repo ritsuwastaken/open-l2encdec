aceedccde7290664d7913c48e74cf5bfef5cb3e3
ritsu <discohijackat125ruemontmarte@proton.me>
2025-11-22 18:35:26 +0000

    Move `XOR` utils, clean up imports

--- src/xor.h
+++ src/xor.h
@@ -12,6 +12,8 @@ size_t encrypt(const std::vector<unsigned char> &input, std::vector<unsigned cha
 size_t encrypt(const std::vector<unsigned char> &input, std::vector<unsigned char> &output, int start_index, KeyGenerator key_generator);
 size_t decrypt(const std::vector<unsigned char> &input, std::vector<unsigned char> &output, int xor_key);
 size_t decrypt(const std::vector<unsigned char> &input, std::vector<unsigned char> &output, int start_index, KeyGenerator key_generator);
+int get_key_by_index(int index);
+int get_key_by_filename(std::string filename);
 } // namespace XOR
 
 #endif // XOR_H
