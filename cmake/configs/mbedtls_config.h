3b5674e4f14a3c29cba204d68e0075aa285994e4
ritsu <discohijackat125ruemontmarte@proton.me>
2025-12-08 19:28:35 +0000

    Fix mingw build

--- cmake/configs/mbedtls_config.h
+++ cmake/configs/mbedtls_config.h
@@ -4,5 +4,8 @@
 #define MBEDTLS_NO_PLATFORM_ENTROPY
 
 #define MBEDTLS_BIGNUM_C
+#if defined(__MINGW32__) || (defined(_MSC_VER) && _MSC_VER <= 1900)
+#define MBEDTLS_PLATFORM_C
+#endif
 
 #endif // MBEDTLS_CONFIG_H
