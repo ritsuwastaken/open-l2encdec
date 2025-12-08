65f57d3fad43636351c46357bb1a5e18164d36df
ritsu <153109555+ritsuwastaken@users.noreply.github.com>
2025-12-08 16:16:34 +0000

    Refactor utils (#27)
    * Refactor utils
    
    * Add tests

---
+++ tests/test_utils.cpp
@@ -0,0 +1,82 @@
+#include "utils.h"
+#include <cstddef>
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+TEST(UtilsHeader, AddHeaderBasic)
+{
+    std::vector<unsigned char> data = {0xAA, 0xBB};
+    utils::add_header(data, "AB");
+
+    std::vector<unsigned char> expected = {
+        0x41, 0x00,
+        0x42, 0x00,
+        0xAA, 0xBB};
+
+    EXPECT_EQ(data, expected);
+}
+
+TEST(UtilsHeader, AddHeaderEmpty)
+{
+    std::vector<unsigned char> data = {0x11, 0x22};
+    utils::add_header(data, "");
+
+    std::vector<unsigned char> expected = {0x11, 0x22};
+    EXPECT_EQ(data, expected);
+}
+
+TEST(UtilsTail, MakeTailBasic)
+{
+    constexpr uint32_t crc = 0x12345678;
+    constexpr size_t offset = 2;
+    constexpr size_t size = 8;
+
+    std::string tail = utils::make_tail(crc, offset, size);
+
+    // Memory layout (little-endian):
+    // Bytes: 00 00 78 56 34 12 00 00
+    // Hex:   0000785634120000
+    EXPECT_EQ(tail, "0000785634120000");
+}
+
+TEST(UtilsTail, AddTailBasic)
+{
+    std::vector<unsigned char> data = {0xAA, 0xBB};
+    utils::add_tail(data, "0102FF");
+
+    std::vector<unsigned char> expected = {
+        0xAA, 0xBB,
+        0x01, 0x02, 0xFF};
+
+    EXPECT_EQ(data, expected);
+}
+
+TEST(UtilsTail, AddTailOddLengthPadding)
+{
+    std::vector<unsigned char> data;
+    utils::add_tail(data, "A");
+
+    std::vector<unsigned char> expected = {0x0A};
+    EXPECT_EQ(data, expected);
+}
+
+TEST(UtilsTail, MakeTailAddTailRoundTrip)
+{
+    constexpr uint32_t crc = 0xDEADBEEF;
+    constexpr size_t offset = 4;
+    constexpr size_t size = 16;
+
+    std::vector<unsigned char> data;
+    utils::add_tail(data, utils::make_tail(crc, offset, size));
+
+    ASSERT_EQ(data.size(), 16u);
+
+    std::vector<unsigned char> expected = {
+        0x00, 0x00, 0x00, 0x00,
+        0xEF, 0xBE, 0xAD, 0xDE,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00};
+
+    EXPECT_EQ(data, expected);
+}
