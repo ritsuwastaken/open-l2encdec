7225a2a80bcc9a1fa9e1d30bb2b8e8dcf56aeba9
ritsu <discohijackat125ruemontmarte@proton.me>
2025-12-08 16:52:34 +0000

    Add tests

---
+++ tests/test_l2encdec_init_params.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include <l2encdec.h>
+
+TEST(L2InitParams, InvalidProtocol)
+{
+    l2encdec::Params p{};
+    EXPECT_FALSE(l2encdec::init_params(p, 999, "", false));
+}
+
+TEST(L2InitParams, XORFilenameRequiresFilename)
+{
+    l2encdec::Params p{};
+    EXPECT_FALSE(l2encdec::init_params(p, 121, "", false));
+}
+
+TEST(L2InitParams, XORBasic)
+{
+    l2encdec::Params p{};
+    ASSERT_TRUE(l2encdec::init_params(p, 111, "", false));
+    EXPECT_EQ(p.type, l2encdec::Type::XOR);
+    EXPECT_EQ(p.xor_key, 0xAC);
+    EXPECT_EQ(p.header, "Lineage2Ver111");
+}
+
+TEST(L2InitParams, RSAOverridesLegacy)
+{
+    l2encdec::Params p{};
+    ASSERT_TRUE(l2encdec::init_params(p, 413, "", false));
+    EXPECT_EQ(p.type, l2encdec::Type::RSA);
+    EXPECT_EQ(p.rsa_private_exponent, "1d");
+}
+
+TEST(L2InitParams, RSAUsesLegacy)
+{
+    l2encdec::Params p{};
+    ASSERT_TRUE(l2encdec::init_params(p, 413, "", true));
+    EXPECT_EQ(p.type, l2encdec::Type::RSA);
+    EXPECT_EQ(p.rsa_private_exponent, "35");
+}
---
+++ tests/test_l2encdec_verify_checksum.cpp
@@ -0,0 +1,44 @@
+#include "utils.h"
+#include "zlib_utils.h"
+#include <gtest/gtest.h>
+#include <l2encdec.h>
+
+constexpr size_t CRC32_OFFSET = 12;
+constexpr size_t FOOTER_SIZE = 20;
+
+TEST(L2Checksum, VerifySuccess)
+{
+    std::vector<unsigned char> data = {'A', 'B', 'C'};
+
+    std::string tail = utils::make_tail(
+        zlib_utils::checksum(data),
+        CRC32_OFFSET,
+        FOOTER_SIZE);
+
+    utils::add_tail(data, tail);
+
+    EXPECT_EQ(
+        l2encdec::verify_checksum(data),
+        l2encdec::ChecksumResult::SUCCESS);
+}
+
+TEST(L2Checksum, VerifyMismatch)
+{
+    std::vector<unsigned char> data = {'A', 'B', 'C'};
+    std::string tail = utils::make_tail(
+        zlib_utils::checksum(data),
+        CRC32_OFFSET,
+        FOOTER_SIZE);
+
+    utils::add_tail(data, tail);
+
+    const size_t base = data.size() - FOOTER_SIZE + CRC32_OFFSET;
+    data[base + 0] ^= 0xFF;
+    data[base + 1] ^= 0xFF;
+    data[base + 2] ^= 0xFF;
+    data[base + 3] ^= 0xFF;
+
+    EXPECT_EQ(
+        l2encdec::verify_checksum(data),
+        l2encdec::ChecksumResult::MISMATCH);
+}
